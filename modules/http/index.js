00e63aa94ba065e2c19f39211d06420b636bac4c
grishaharutyunyan <grishaharutyunian@gmail.com>
2024-07-01 22:14:22 +0400

    http module

--- modules/http/index.js
+++ modules/http/index.js
@@ -1,23 +1,47 @@
-import { createServer  } from 'http'
-// "type": "module", this add in package.json for using import , if not use require
+import { createServer } from 'http';
+import fs from 'fs';
+import path from 'path';
 
+const mimeType = {
+    ".html": 'text/html',
+    ".css": 'text/css',
+    ".js": 'text/javascript',
+};
 
-const server  = createServer((req,res)=> {
-
-    if(req.url === "/hello") {
-        res.writeHead(200, {"Contnet-Type": "text/plain"});
-        res.end("hello");
-    }else{
-        res.writeHead(200, {"Contnet-Type": "text/plain"});
-        res.end("bye");
+function fileMiddleware(req, res, next) {
+    let url = req.url;
+    if (url === "/") {
+        url = "/index.html";
     }
-    // res.writeHead(200,{"Content-Type" : "text/plain"})
-    // res.end("Hello Armenia")
-    // res.writeHead(200,{"Content-Type" : "application/json"})
-    // res.end(JSON.stringify({
-    //     name : "Joe"
-    // }))
-})
+    const filePath = path.resolve('public' + url);
+    fs.promises.access(filePath)
+        .then(() => {
+            const ext = path.extname(filePath);
+            res.writeHead(200, { 'Content-Type': mimeType[ext] });
+            fs.createReadStream(filePath).pipe(res);
+        })
+        .catch(() => {
+            next();
+        });
+}
+
+const server = createServer((req, res) => {
+    fileMiddleware(req, res, () => {
+        if (req.url === '/') {
+            res.writeHead(200, { 'Content-Type': 'text/html' });
+            fs.createReadStream(path.resolve("public/index.html")).pipe(res);
+        } else if (req.url === '/style.css') {
+            res.writeHead(200, { 'Content-Type': 'text/css' });
+            fs.createReadStream(path.resolve("public/style.css")).pipe(res);
+        } else if (req.url === "/hello") {
+            res.writeHead(200, { "Content-Type": "text/plain" });
+            res.end("hello");
+        } else {
+            res.writeHead(200, { "Content-Type": "text/plain" });
+            res.end("bye");
+        }
+    });
+});
 
 server.listen(3001, () => {
     console.log("Server running at http://localhost:3001/");
