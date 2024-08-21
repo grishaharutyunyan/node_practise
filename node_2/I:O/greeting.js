6b9977a6fd997d92744aca4941eed52eef191736
grishaharutyunyan <grishaharutyunian@gmail.com>
2024-08-21 18:09:23 +0400

    fs

---
+++ node_2/I:O/greeting.js
@@ -0,0 +1,9 @@
+console.log("What is your name?");
+process.stdin.on("data", (data) => {
+  const name = data.toString().trim().toUpperCase();;
+  if (name !== "") {
+    process.stdout.write(`Hello ${name}!`);
+  } else {
+    process.stderr.write("Input was empty.");
+  }
+});
